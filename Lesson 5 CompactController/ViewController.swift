c88f22dce661640884883d960c997e67a19c3e2f
LBkos <constantin.l.o.p@gmail.com>
2023-03-13 15:42:45 +0700

    Initial Commit

---
+++ Lesson 5 CompactController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 5 CompactController
+//
+//  Created by Константин Лопаткин on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
